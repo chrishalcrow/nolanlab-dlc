adb0a867cb3a18ee4e7286fb6d55b334b3a513df
chrishalcrow <chrishalcrow@gmail.com>
2025-11-07 11:17:57 +0000

    and so it begins

---
+++ main.py
@@ -0,0 +1,45 @@
+from argparse import ArgumentParser
+from pathlib import Path
+import deeplabcut as dlc
+import pandas as pd
+
+def main():
+
+    parser = ArgumentParser()
+
+    parser.add_argument('mouse')
+    parser.add_argument('day')
+    parser.add_argument('session')
+    parser.add_argument('--data_folder', default=None)
+    parser.add_argument('--deriv_folder', default=None)
+
+    mouse = int(parser.parse_args().mouse)
+    day = int(parser.parse_args().day)
+    session = parser.parse_args().session
+
+    data_folder = parser.parse_args().data_folder
+    if data_folder is None:
+        data_folder = "/home/nolanlab/Work/Harry_Project/data/"
+    data_folder = Path(data_folder)
+
+    deriv_folder = parser.parse_args().deriv_folder
+    if deriv_folder is None:
+        deriv_folder = "/home/nolanlab/Work/Harry_Project/derivatives/"
+    deriv_folder = Path(deriv_folder)
+
+    config_path = "/exports/eddie/scratch/chalcrow/wolf/code/models/of_cohort12-krs-2024-10-30/config.yaml"
+
+    path_to_all_filepaths = "/exports/eddie/scratch/chalcrow/wolf/code/nolanlab-ephys/all_filepaths.csv"
+    all_filepaths = pd.read_csv(path_to_all_filepaths)
+    mouseday_folder = all_filepaths.query(f'mouse == {mouse} & day == {day} & session == "{session}"')['filepath'].values[0]
+
+    video_path = mouseday_folder / f"sub-{mouse}_day-{day}_ses-{session}_video.avi"
+    save_path = deriv_folder / f"M{mouse}/D{day}/{session}/dlc_output"
+
+    dlc.analyze_videos(config_path, [video_path], save_as_csv=True, destfolder=save_path)
+    dlc.filterpredictions(config_path, [video_path])
+    dlc.create_labeled_video(config_path, [video_path], save_frames=False)
+    dlc.plot_trajectories(config_path, [video_path])
+
+if __name__ == "__main__":
+    main()
