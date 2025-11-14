a19ddae1df247f582ad4014f4eaafd5db63b486a
chrishalcrow <chrishalcrow@gmail.com>
2025-11-14 10:16:14 +0000

    add cropping

--- dlc_on_comp.py
+++ dlc_on_comp.py
@@ -4,6 +4,44 @@ import deeplabcut as dlc
 import shutil
 import os
 import pandas as pd
+import cv2
+
+def make_cropped_video(video_path, output_path, cropping):
+    
+    if cropping is None:
+        _ = shutil.copy(video_path, output_path)
+
+    cap = cv2.VideoCapture(video_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    # Initialize frame counter
+    cnt = 0
+    
+    x,y,w,h = cropping
+
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(output_path, fourcc, fps, (w, h))
+
+    # Now we start
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+
+        cnt += 1 
+
+        if ret==True:
+            crop_frame = frame[y:y+h, x:x+w]
+
+            out.write(crop_frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            break
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
 
 def main():
 
@@ -34,7 +72,6 @@ def main():
         deriv_folder = "/exports/eddie/scratch/chalcrow/wolf/derivatives"
     deriv_folder = Path(deriv_folder)
 
-
     if bodypart == "lick":
         config_path = "/exports/eddie/scratch/chalcrow/wolf/code/models/c12_lick-chris-2024-10-03/config.yaml"
     elif bodypart == "eye":
@@ -45,35 +82,35 @@ def main():
     mouse_day_session_folder = list(data_folder.glob(f'M{mouse:02d}_D{day:02d}_*{session}'))[0]
 
     video_path = str(mouse_day_session_folder / f"M{mouse:02d}_D{day:02d}_{session}_side_capture.avi")
-    video_filename = video_path.split("/")[-1]
+    cropped_video_path = str(mouse_day_session_folder / f"M{mouse:02d}_D{day:02d}_{session}_side_capture_{bodypart}.avi")
+
     save_path = deriv_folder / f"M{mouse:02d}/D{day:02d}/{session}/dlc_output_{bodypart}/"
     save_path.mkdir(parents=True, exist_ok=True)
     save_path = str(save_path)
 
-    derivatives_video_path = save_path + "/" + video_filename
-    _ = shutil.copy(video_path, derivatives_video_path)
+    #derivatives_video_path = save_path + "/" + video_filename
+    #_ = shutil.copy(video_path, derivatives_video_path)
 
     if bodypart in ["eye", "tongue"]:
         all_crop_info = pd.read_csv(f"wolf_crops/{bodypart}_crops_wolf.csv")
         x,y,w,h = all_crop_info.query(f'mouse == {mouse} & day == {day}')[['x','y','w','h']].values[0]
-        cropping  =  [x, x+w, y, y+h]
+        cropping  =  [x, y, w, h]
     else:
         cropping = None
 
-    print(f"{cropping=}")
+    make_cropped_video(video_path, cropped_video_path, cropping)
 
     dlc.analyze_videos(
         config_path, 
-        [derivatives_video_path], 
+        [cropped_video_path], 
         save_as_csv=True, 
         destfolder=save_path,
-        cropping = cropping, 
     )
-    dlc.filterpredictions(config_path, [derivatives_video_path])
-    dlc.create_labeled_video(config_path, [derivatives_video_path], save_frames=False)
-    dlc.plot_trajectories(config_path, [derivatives_video_path])
+    dlc.filterpredictions(config_path, [cropped_video_path])
+    dlc.create_labeled_video(config_path, [cropped_video_path], save_frames=False)
+    dlc.plot_trajectories(config_path, [cropped_video_path])
 
-    os.remove(derivatives_video_path)
+    os.remove(cropped_video_path)
 
 if __name__ == "__main__":
     main()
