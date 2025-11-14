c7839c2e34fe4ace4e34d791e451c23df01da46a
chrishalcrow <chrishalcrow@gmail.com>
2025-11-14 09:22:22 +0000

    add stageout

--- dlc_on_eddie.py
+++ dlc_on_eddie.py
@@ -1,4 +1,4 @@
-from eddie_helper.make_scripts import run_python_script
+from eddie_helper.make_scripts import run_python_script, run_stage_script
 
 from argparse import ArgumentParser
 from pathlib import Path
@@ -20,16 +20,26 @@ def main():
     bodypart = parser.parse_args().bodypart
 
     data_folder = parser.parse_args().data_folder
+    if data_folder is None:
+        data_folder = "/exports/eddie/scratch/chalcrow/wolf/data"
+    data_folder = Path(data_folder)
+
     deriv_folder = parser.parse_args().deriv_folder
+    if deriv_folder is None:
+        deriv_folder = "/exports/eddie/scratch/chalcrow/wolf/derivatives"
+    deriv_folder = Path(deriv_folder)
 
+    run_python_name = f"M{mouse}D{day}{session[:2]}{bodypart}"
+    stageout_job_name = f"M{mouse}D{day}{session[:2]}out" 
 
-    run_python_name = f"M{mouse}D{day}{session}dlc"
-#    stageout_job_name = f"M{mouse}D{day}{sessions[0]}out" 
+    eddie_active_projects = Path("/exports/cmvm/datastore/sbms/groups/CDBS_SIDB_storage/NolanLab/ActiveProjects/")
+    stageout_dict = {deriv_folder / f"M{mouse:02d}/D{day:02d}/{session}/dlc_output_{bodypart}": eddie_active_projects / "Chris/Wolf_Experiment/derivatives" / f"M{mouse:02d}/D{day:02d}/{session}/"}
 
     uv_directory = "/exports/eddie/scratch/chalcrow/wolf/code/nolanlab-dlc/"
     python_arg = f"dlc_on_comp.py {mouse} {day} {session} {bodypart} {data_folder} {deriv_folder}"
 
     run_python_script(uv_directory, python_arg, cores=8, email="chalcrow@ed.ac.uk", staging=False, job_name=run_python_name)
+    run_stage_script(stageout_dict, job_name=stageout_job_name, hold_jid=run_python_name)
 
 if __name__ == "__main__":
     main()
